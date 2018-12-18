219750486dfc7f6726b041ce47f73ebceb7b241e
Stephen Dunne D <stephen.d.dunne@ericsson.com>
2018-12-18 17:09:42 +0000

    Updating DetailedDiff to handle errors

--- src/detailed/index.js
+++ src/detailed/index.js
@@ -2,10 +2,18 @@ import addedDiff from '../added';
 import deletedDiff from '../deleted';
 import updatedDiff from '../updated';
 
-const detailedDiff = (lhs, rhs) => ({
-  added: addedDiff(lhs, rhs),
-  deleted: deletedDiff(lhs, rhs),
-  updated: updatedDiff(lhs, rhs),
-});
+const detailedDiff = function (lhs, rhs) {
+  var finalDiff = {};
+  try {
+    finalDiff.added = addedDiff(lhs, rhs);
+    finalDiff.deleted = deletedDiff(lhs, rhs);
+    finalDiff.updated = updatedDiff(lhs, rhs);
+  } catch (errDetailedDiff) {
+    finalDiff.error = errDetailedDiff
+    console.log(errDetailedDiff);
+  } finally {
+    return finalDiff;
+  }
+);
 
 export default detailedDiff;
